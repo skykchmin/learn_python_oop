508d8e1a6113fe5c1c155d040ace37cdad036624
skykchmin <skykchmin@naver.com>
2022-04-19 23:29:54 +0900

    [220419] Feature - refactoring_encapsulation

--- oop/00_decorator.py
+++ oop/00_decorator.py
@@ -27,3 +27,14 @@ def love():
 smile()
 angry()
 love()
+
+def counter():
+    i = 0
+    def count():
+        nonlocal i
+        i += 1
+        return i
+    return count
+c = counter()
+for i in range(10):
+    print(c(), end = ' ')
